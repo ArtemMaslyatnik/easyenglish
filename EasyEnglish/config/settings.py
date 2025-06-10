f1fc1c3ab01a0d095a3ef8e89971c1bae5e86fb3
Lykov Artem <lykav2999@gmail.com>
2025-06-10 07:13:57 +0300

    Django collectstatic

--- EasyEnglish/config/settings.py
+++ EasyEnglish/config/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
-from privatekey import KEY_DJANGO
 from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
