577997f838fb935a71ee8c4f0da0abb4be7e9e03
Lykov Artem <lykav2999@gmail.com>
2025-06-29 17:08:48 +0300

    craate text analysis

--- EasyEnglish/config/settings.py
+++ EasyEnglish/config/settings.py
@@ -172,6 +172,7 @@ AUTHENTICATION_BACKENDS = (
     'social_core.backends.google.GoogleOAuth2',
     'social_core.backends.twitter.TwitterOAuth',
     'django.contrib.auth.backends.ModelBackend',
+    'users.authentication.EmailAuthBackend',
 )
 #PosgreSQL
 SOCIAL_AUTH_JSONFIELD_ENABLED = True
