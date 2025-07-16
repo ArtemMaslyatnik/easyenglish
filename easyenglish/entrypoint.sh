eedb28502a6578709c22ad155484308284a68e6a
Lykov Artem <lykav2999@gmail.com>
2025-07-16 05:50:32 +0300

    clean git

--- easyenglish/entrypoint.sh
+++ easyenglish/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Collect static files
 #echo "Collect static files"
 #python3 manage.py collectstatic --noinput
@@ -10,7 +11,7 @@ python3 manage.py migrate --no-input
 if [ "$DJANGO_DEBUG" = "true" ]
 then
     echo "Running Django in debug mode"
-    python -m debugpy --listen 0.0.0.0:5678 manage.py runserver 0.0.0.0:8000
+    python3 -m debugpy --listen 0.0.0.0:5678 manage.py runserver 0.0.0.0:8000
 else
    echo "Running Django in production mode"
    exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3
